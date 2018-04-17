ca984ddfb764d0307ef9baaff8e5a8a1b45a959a
ZavodPodushek <new_mouse@mail.ru>
2018-04-17 20:21:29 +0300

    Rewrite log route

--- routes/api.log.js
+++ routes/api.log.js
@@ -3,31 +3,49 @@ const fs = require('fs');
 function log() {
     app.get('/log', (req, res) => {
         var resultPath = './log/result.log';
-        fs.readdir('./log', (err, data) => {
-            if (err) {
-                res.end(null, 'utf8')
-            }
-            var resultString = '';
-            var target = data.filter(data => data.split('.')[0].length === 10).slice(-3);
-            target.forEach(file => {
-                console.log('./log/' + file);
-                resultString += fs.readFileSync('./log/' + file, 'utf8');
+        new Promise((res,rej) =>{
+            fs.readdir('./log',(err,data) =>{
+                if(err) {
+                    rej(err)
+                } else {
+                    const resultArray = [];
+                    var target = data.filter(data => data.split('.')[0].length === 10).slice(-3);
+                    target.forEach(file => resultArray.push(fs.readFile('./log/' + file, 'utf8',(err, data) => {
+                        res(data)
+                    } )));
+                    return Promise.all(resultArray)
+                }
             })
-            fs.writeFile(resultPath, resultString, 'utf8', (err) => {
-                if (err) {
-                    res.end(null, 'utf8')
-                }
-            });
-            fs.readFile(resultPath, (err, data) => {
-                if (err) {
-                    logger.error(err.errmsg);
-                    res.end(err, 'utf8');
-                }
-                res.end(data, 'utf8')
-            });
-            console.log('Ready for reading, all logs ion .log/result.log')
+        }).then(resultArray => {
+            res.send(JSON.stringify(resultArray));
+            console.log('JSON successfully send')
         })
     });
 }
 
 module.exports = log;
+
+/*fs.readdir('./log', (err, data) => {
+            if (err) {
+                res.send(null)
+            }
+            var resultString = '';
+            var target = data.filter(data => data.split('.')[0].length === 10).slice(-3);
+            target.forEach(file => {
+                console.log('./log/' + file);
+
+            })
+            fs.writeFile(resultPath, resultString, 'utf8', (err) => {
+                if (err) {
+                    res.send(null)
+                }
+            });
+            fs.readFile(resultPath, 'utf8', (err, data) => {
+                if (err) {
+                    logger.error(err.errmsg);
+                    res.send(err);
+                }
+                res.end(JSON.stringify(data))
+            });
+            console.log('Ready for reading, all logs ion .log/result.log')
+        })*/
